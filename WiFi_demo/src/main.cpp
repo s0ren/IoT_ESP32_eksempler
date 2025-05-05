989426ac61246981fe084e813cc43c8f1295ff69
Søren Magnusson <smag@tec.dk>
2025-05-05 10:56:21 +0200

    Wifi demo

---
+++ WiFi_demo/src/main.cpp
@@ -0,0 +1,77 @@
+
+// Se https://randomnerdtutorials.com/solved-reconnect-esp32-to-wifi/
+// og https://randomnerdtutorials.com/esp32-ntp-timezones-daylight-saving/#more-107914
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
+
+// Replace with your network credentials (STATION)
+// const char* ssid = "IoT_Emb";
+// const char* password = "98806829";
+
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+
+void setTimezone(String timezone){
+  Serial.printf("  Setting Timezone to %s\n",timezone.c_str());
+  setenv("TZ",timezone.c_str(),1);  //  Now adjust the TZ.  Clock settings are adjusted to show the new local time
+  tzset();
+}
+
+void initTime(String timezone){
+  struct tm timeinfo;
+
+  Serial.println("Setting up time");
+  configTime(0, 0, "pool.ntp.org");    // First connect to NTP server, with 0 TZ offset
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("  Failed to obtain time");
+    return;
+  }
+  Serial.println("  Got the time from NTP");
+  // Now we can set the real timezone
+  setTimezone(timezone);
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time 1");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S zone %Z %z ");
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+
+  initTime("CET-1CEST,M3.5.0,M10.5.0/3");   // Set for Europe/Copenhagen
+  printLocalTime();
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+  // if WiFi is down, try reconnecting every CHECK_WIFI_TIME seconds
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >=interval)) {
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    previousMillis = currentMillis;
+  }
+}
